54a69bf4d6edd28f89aab23ff2b9ae2fcae704cd
Georgios Keramidas <georgiosmidas@gmail.com>
2023-10-18 14:40:21 +0100

    Split Dashboard page into tabs

--- 0_🏠_Dashboard.py
+++ 0_🏠_Dashboard.py
@@ -5,67 +5,70 @@ import streamlit as st
 
 st.markdown("# Perfin")
 
-col1, col2 = st.columns([1, 1], gap="medium")
+tab1, tab2 = st.tabs(["Settings", "Tips"])
 
-with col1:
-  st.markdown("### Tips 💡")
-  st.markdown("""
-  - Update this app at the end of every month :sparkles:
-  - Review your allocation in 
-    - [Debt payments](/Debt)
-    - [Bills](/Bills)
-    - [Tansportation](/Transportation)
-    - [Investments](/Investments)
-    - [Savings](/Monthly_plan) (in the Monthly Plan)
-  - Add this month's
-    - [Income](/Income)
-    - [Spending](/Spending)
-  - Done! :tada:
-  """)
-  st.markdown("---")
-
+with tab1:
   st.markdown("### Settings ⚙️")
+  col1, col2 = st.columns([1, 1], gap="medium")
+  with col1:
+    st.markdown("#### Global")
+    selected_currency=st.selectbox("Currency:", options=("EUR","GBP","USD"))
+    if "selected_currency" not in st.session_state:
+        st.session_state["selected_currency"] = selected_currency
+    if selected_currency == "EUR":
+        curr_symbol = "€"
+    elif selected_currency == "GBP":
+        curr_symbol = "£"
+    elif selected_currency == "GBP":
+        curr_symbol = "$"
+    else:
+        curr_symbol = selected_currency
+    st.write("Symbol:", curr_symbol)
+    if "curr_symbol" not in st.session_state:
+        st.session_state["curr_symbol"] = curr_symbol
   
-  st.markdown("##### Global")
-  
-  selected_currency=st.selectbox("Currency:", options=("EUR","GBP","USD"))
-  if "selected_currency" not in st.session_state:
-      st.session_state["selected_currency"] = selected_currency
-  if selected_currency == "EUR":
-      curr_symbol = "€"
-  elif selected_currency == "GBP":
-      curr_symbol = "£"
-  elif selected_currency == "GBP":
-      curr_symbol = "$"
-  else:
-      curr_symbol = selected_currency
-  st.write("Symbol:", curr_symbol)
-  if "curr_symbol" not in st.session_state:
-      st.session_state["curr_symbol"] = curr_symbol
+  with col2:
+    st.markdown("#### Assets")
+    price_update_method = st.selectbox(
+        "Price update method:",
+        help="Choose which price you would like to fetch for assets.",
+        options=(
+            "lastPrice",
+            "previousClose",
+            "fiftyDayAverage",
+            "twoHundredDayAverage",
+            "yearLow"
+        ),
+    )
+    if "price_update_method" not in st.session_state:
+      st.session_state["price_update_method"] = price_update_method
 
-  st.markdown("##### Assets")
+    cgt_base=st.number_input(
+        "Capital Gains Tax (%):",
+        min_value = 0,
+        max_value = 100,
+        value = 33,
+        format = "%d",
+    )
+    cgt = cgt_base/100
+    if "cgt" not in st.session_state:
+      st.session_state["cgt"] = cgt
 
-  price_update_method = st.selectbox(
-      "Price update method:",
-      help="Choose which price you would like to fetch for assets.",
-      options=(
-          "lastPrice",
-          "previousClose",
-          "fiftyDayAverage",
-          "twoHundredDayAverage",
-          "yearLow"
-      ),
-  )
-  if "price_update_method" not in st.session_state:
-    st.session_state["price_update_method"] = price_update_method
+with tab2:
+  col1, col2 = st.columns([1, 1], gap="medium")
 
-  cgt_base=st.number_input(
-      "Capital Gains Tax (%):",
-      min_value = 0,
-      max_value = 100,
-      value = 33,
-      format = "%d",
-  )
-  cgt = cgt_base/100
-  if "cgt" not in st.session_state:
-    st.session_state["cgt"] = cgt
+  with col1:
+    st.markdown("### Tips 💡")
+    st.markdown("""
+    - Update this app at the end of every month :sparkles:
+    - Review your allocation in 
+      - [Debt payments](/Debt)
+      - [Bills](/Bills)
+      - [Tansportation](/Transportation)
+      - [Investments](/Investments)
+      - [Savings](/Monthly_plan) (in the Monthly Plan)
+    - Add this month's
+      - [Income](/Income)
+      - [Spending](/Spending)
+    - Done! :tada:
+    """)
