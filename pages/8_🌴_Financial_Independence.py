bd83a197d6170a523c8787677cc06fef69072b94
Georgios Keramidas <georgiosmidas@gmail.com>
2023-10-19 23:25:47 +0100

    Add column for better readability

--- pages/8_🌴_Financial_Independence.py
+++ pages/8_🌴_Financial_Independence.py
@@ -26,27 +26,31 @@ with tab1:
     st.write("Months of all necessary expenses covered:", 2)
 
 with tab2:
-    st.write("##### Retirement")
-    st.number_input("Withrawal rate (%):", min_value=0)
-    st.write("Current Capital Gains Tax:", cgt * 100, "%")
-    st.number_input("Expected CGT in retirement (%):", min_value=0)
-    st.number_input('Desired monthly "income":', min_value=0)
-    st.number_input('Desired yearly "income":', min_value=0)
-    st.write("You can retire when your Net investments reach", curr_symbol, 1000000)
-    st.write("Years untill retirement:", curr_symbol, 100)
-    st.divider()
-    st.write("If you retired now:")
-    st.write('Monthly "salary":', curr_symbol, 100)
-    st.write('Yearly "salary":', curr_symbol, 1200)
+    col1, col2 = st.columns([1, 1], gap="medium")
+    with col1:
+        st.write("##### Retirement")
+        st.number_input("Withrawal rate (%):", min_value=0)
+        st.number_input("Expected return rate (from all types of investments) (%):", min_value=0)
+        st.write("Current Capital Gains Tax:", cgt * 100, "%")
+        st.number_input("Expected CGT in retirement (%):", min_value=0)
+        st.number_input('Desired monthly "income":', min_value=0)
+        st.number_input('Desired yearly "income":', min_value=0)
+        st.write("You can retire when your Net investments reach", curr_symbol, 1000000)
+        st.write("Years untill retirement:", curr_symbol, 100)
+        st.divider()
+        st.write("If you retired now:")
+        st.write('Monthly "salary":', curr_symbol, 100)
+        st.write('Yearly "salary":', curr_symbol, 1200)
 
 with tab3:
     st.write("##### Financial status")
     st.write("Net Worth:", curr_symbol, assets_net_worth)
     st.write("Net Investments:", assets_shares_net)
     st.write("Yearly added investments:", 0)
-    st.number_input("Expected return rate (from all types of investments) (%):", min_value=0)
     st.divider()
-    st.write("Financial Status:", [
+    # st.column_config.ProgressColumn("Financial progress")
+    st.write("Financial Status:")
+    st.write([
         "Financial Dependence",
         "Financial Solvancy",
         "Financial Stability",
@@ -64,7 +68,6 @@ with tab3:
 with tab4:
     st.write("##### Evaluate your plan")
     st.link_button("Rich, broke or dead?", "https://engaging-data.com/will-money-last-retire-early")
-    st.write("##### Other resources")
 
 with tab5:
     st.write("##### Other")
