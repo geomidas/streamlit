8de37fa2cc451d0286baee9bea611e81ec654612
Georgios Keramidas <georgiosmidas@gmail.com>
2023-10-20 13:53:31 +0100

    Fix Income table additions

--- pages/5_💰_Income.py
+++ pages/5_💰_Income.py
@@ -11,28 +11,44 @@ st.write("### Income 💰")
 with st.expander("ℹ️ Track your actual monthly income."):
     st.write("This acts as a guide for setting more accurate values in Monthly Outflows.")
 
-df = pd.DataFrame(
-    [
-       {"Date": "2023-10-1", "Net income": 2500, "Health insurance": 0, "Pension contribution":100, "Bonus": 0},
-       {"Date": "2023-09-1", "Net income": 2500, "Health insurance": 0, "Pension contribution":100, "Bonus": 0},
-       {"Date": "2023-08-1", "Net income": 2500, "Health insurance": 0, "Pension contribution":100, "Bonus": 0},
+if 'data' not in st.session_state:
+    data=pd.DataFrame({
+        "Date": [],
+        "Net income": [],
+        "Health insurance": [],
+        "Pension": [],
+        "Bonus": []
+    })
+    st.session_state.data = data
 
-   ]
-)
+def add_df_form():
+    row = pd.DataFrame({
+        'Date': [st.session_state.input_incume_date],
+        'Net income': [st.session_state.input_income_net],
+        'Health insurance': [st.session_state.input_income_health],
+        'Pension': [st.session_state.input_income_pension],
+        'Bonus': [st.session_state.input_income_bonus],
+    })
+    st.session_state.data = pd.concat([st.session_state.data, row])
 
-with st.form("new_income", clear_on_submit=False):
+new_income = st.form(key="new_income", clear_on_submit=False)
+with new_income:
     st.write("#### Add income data (in " + curr_symbol + ')')
-    col1, col2 = st.columns([1, 1], gap="medium")
-    with col1:
-        date = str(st.date_input("Date", format="YYYY-MM-DD"))
-        income_net = st.number_input("Net income", min_value=0, step=1)
-    with col2:
-        income_health = st.number_input("Health insurance", min_value=0, step=1)
-        income_pension = st.number_input("Pension contribution", min_value=0, step=1)
-        income_bonus = st.number_input("Bonus", min_value=0, step=1)
-    submitted = st.form_submit_button("Submit", help="Adds the data to the table below.")
+
+    df_form_columns = st.columns(5)
+    with df_form_columns[0]:
+        st.date_input("Date", format="YYYY-MM-DD", key="input_incume_date")
+    with df_form_columns[1]:
+        st.number_input("Net income", min_value=0, step=1, key="input_income_net")
+    with df_form_columns[2]:
+        st.number_input("Health insurance", min_value=0, step=1, key="input_income_health")
+    with df_form_columns[3]:
+        st.number_input("Pension", min_value=0, step=1, key="input_income_pension")
+    with df_form_columns[4]:
+        st.number_input("Bonus", min_value=0, step=1, key="input_income_bonus")
+    
+    submitted = st.form_submit_button("Submit", help="Adds the data to the table below.", on_click=add_df_form)
     if submitted:
-       df.add({"Date": date, "Net income": income_net, "Health insurance": income_health, "Pension contribution": income_pension, "Bonus": income_bonus})
        st.toast("Updated Income table 💰", icon="🎉")
 
-st.dataframe(df, hide_index=True, width=720)
+st.dataframe(st.session_state.data, hide_index=True, width=720)
