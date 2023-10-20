923b807f6d2343beedfd6a49b2c751dd4d9f5cf8
Georgios Keramidas <georgiosmidas@gmail.com>
2023-10-20 10:48:50 +0100

    Improve income and spending pages

--- pages/5_💰_Income.py
+++ pages/5_💰_Income.py
@@ -1,17 +1,38 @@
 import streamlit as st
 import pandas as pd
 
+
 st.set_page_config(layout="centered")
 
-st.markdown("# Perfin")
-st.markdown("### Income 💰")
-st.markdown("Track your actual monthly income. This acts as a guide for setting more accurate values in Monthly Outflows.")
+curr_symbol = st.session_state["curr_symbol"]
+
+st.write("# Perfin")
+st.write("### Income 💰")
+with st.expander("ℹ️ Track your actual monthly income."):
+    st.write("This acts as a guide for setting more accurate values in Monthly Outflows.")
 
 df = pd.DataFrame(
     [
-       {"Month":"2023-10", "Gross":40000, "Tax":1000, "Health Ins.":100, "Net":2500, "Pension":100, "Bonus":0},
-       {"Month":"2023-09", "Gross":40000, "Tax":1000, "Health Ins.":100, "Net":2500, "Pension":100, "Bonus":0},
-       {"Month":"2023-08", "Gross":40000, "Tax":1000, "Health Ins.":100, "Net":2500, "Pension":100, "Bonus":0},
+       {"Date": "2023-10-1", "Net income": 2500, "Health insurance": 0, "Pension contribution":100, "Bonus": 0},
+       {"Date": "2023-09-1", "Net income": 2500, "Health insurance": 0, "Pension contribution":100, "Bonus": 0},
+       {"Date": "2023-08-1", "Net income": 2500, "Health insurance": 0, "Pension contribution":100, "Bonus": 0},
+
    ]
 )
-st.table(df)
+
+with st.form("new_income", clear_on_submit=False):
+    st.write("#### Add income data (in " + curr_symbol + ')')
+    col1, col2 = st.columns([1, 1], gap="medium")
+    with col1:
+        date = str(st.date_input("Date", format="YYYY-MM-DD"))
+        income_net = st.number_input("Net income", min_value=0, step=1)
+    with col2:
+        income_health = st.number_input("Health insurance", min_value=0, step=1)
+        income_pension = st.number_input("Pension contribution", min_value=0, step=1)
+        income_bonus = st.number_input("Bonus", min_value=0, step=1)
+    submitted = st.form_submit_button("Submit", help="Adds the data to the table below.")
+    if submitted:
+       df.add({"Date": date, "Net income": income_net, "Health insurance": income_health, "Pension contribution": income_pension, "Bonus": income_bonus})
+       st.toast("Updated Income table 💰", icon="🎉")
+
+st.dataframe(df, hide_index=True, width=720)
--- pages/6_💸_Spending.py
+++ pages/6_💸_Spending.py
@@ -3,17 +3,47 @@ import pandas as pd
 
 st.set_page_config(layout="centered")
 
-st.markdown("# Perfin")
-st.markdown("### Spending 💸")
-st.markdown("Track your actual monthly spending. This acts as a guide for setting more accurate values in Monthly Outflows.")
+curr_symbol = st.session_state["curr_symbol"]
 
-st.form("Add new data")
+st.write("# Perfin")
+st.write("### Spending 💸")
+with st.expander("ℹ️ Track your actual monthly spending."):
+    st.write("This acts as a guide for setting more accurate values in Monthly Outflows.")
 
-df = pd.DataFrame(
+df_spend = pd.DataFrame(
     [
-        {"Month":"2023-10", "Rent+Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
-        {"Month":"2023-09", "Rent+Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
-        {"Month":"2023-08", "Rent+Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
+        {"Date":"2023-10-1", "Rent+Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
+        {"Date":"2023-09-1", "Rent+Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
+        {"Date":"2023-08-1", "Rent+Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
  ]
 )
-st.table(df)
+
+with st.form("new_spend", clear_on_submit=False):
+    st.write("#### Add spending data (in " + curr_symbol + ')')
+    col1, col2 = st.columns([1, 1], gap="medium")
+    with col1:
+        date = str(st.date_input("Date", format="YYYY-MM-DD"))
+        spend_rent = st.number_input("Rent+Bills", min_value=0, step=1)
+        spend_pension = st.number_input("Pension", min_value=0, step=1)
+        spend_transp = st.number_input("Transportation", min_value=0, step=1)
+    with col2:
+        spend_fun = st.number_input("Food+Fun", min_value=0, step=1)
+        spend_invest = st.number_input("Investments", min_value=0, step=1)
+        spend_shop = st.number_input("Shopping", min_value=0, step=1)
+        spend_travel = st.number_input("Travel expenses", min_value=0, step=1)
+    submitted = st.form_submit_button("Submit", help="Adds the data to the table below.")
+    if submitted:
+       df_spend.add({
+           "Date": date, 
+            "Rent + Bills": spend_rent, 
+            "Pension": spend_pension, 
+            "Transportation": spend_transp,
+            "Food + Fun": spend_fun,
+            "Investments": spend_invest,
+            "Shopping": spend_shop,
+            "Travel": spend_travel,
+        })
+       st.toast("Updated Spending table 💸", icon="🎉")
+
+
+st.dataframe(df_spend, hide_index=True, width=720)
