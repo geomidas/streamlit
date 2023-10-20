73ef38ff1acabfaecec954a4e5ef2de01acbbb7a
Georgios Keramidas <georgiosmidas@gmail.com>
2023-10-20 12:03:56 +0100

    Minor improvements

--- pages/6_💸_Spending.py
+++ pages/6_💸_Spending.py
@@ -12,9 +12,9 @@ with st.expander("ℹ️ Track your actual monthly spending."):
 
 df_spend = pd.DataFrame(
     [
-        {"Date":"2023-10-1", "Rent+Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
-        {"Date":"2023-09-1", "Rent+Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
-        {"Date":"2023-08-1", "Rent+Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
+        {"Date":"2023-10-1", "Rent + Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
+        {"Date":"2023-09-1", "Rent + Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
+        {"Date":"2023-08-1", "Rent + Bills":1100, "Pension":100, "Transportation":10, "Food+Fun":500, "Investments":100, "Shopping":50, "Travel":0},
  ]
 )
 
@@ -23,11 +23,11 @@ with st.form("new_spend", clear_on_submit=False):
     col1, col2 = st.columns([1, 1], gap="medium")
     with col1:
         date = str(st.date_input("Date", format="YYYY-MM-DD"))
-        spend_rent = st.number_input("Rent+Bills", min_value=0, step=1)
+        spend_rent = st.number_input("Rent + Bills", min_value=0, step=1)
         spend_pension = st.number_input("Pension", min_value=0, step=1)
         spend_transp = st.number_input("Transportation", min_value=0, step=1)
     with col2:
-        spend_fun = st.number_input("Food+Fun", min_value=0, step=1)
+        spend_fun = st.number_input("Food + Fun", min_value=0, step=1)
         spend_invest = st.number_input("Investments", min_value=0, step=1)
         spend_shop = st.number_input("Shopping", min_value=0, step=1)
         spend_travel = st.number_input("Travel expenses", min_value=0, step=1)
@@ -44,6 +44,5 @@ with st.form("new_spend", clear_on_submit=False):
             "Travel": spend_travel,
         })
        st.toast("Updated Spending table 💸", icon="🎉")
-
 
 st.dataframe(df_spend, hide_index=True, width=720)
