7dc814a76329fd2131591efeb8b7790b45c4df15
Georgios Keramidas <georgiosmidas@gmail.com>
2023-11-10 14:14:07 +0000

    Add firebase module

--- 1_🏠_1._Home.py
+++ 1_🏠_1._Home.py
@@ -76,7 +76,8 @@ else:
             st.info(
                 "__Email:__ `" + st.session_state.user_info["email"] + "`\n\n" + 
                 "__Verified:__ `" + str(st.session_state.user_info["emailVerified"]) + "`\n\n" +
-                "__User ID:__ `" + st.session_state.user_info["localId"] + "`")
+                "__User ID:__ `" + st.session_state.user_info["localId"] + "`"
+            )
         with col2:
             # Sign out
             st.write("Sign out:")
