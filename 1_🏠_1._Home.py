50dd704e2e5e8d57a8a19255a73d142406b8d69e
Georgios Keramidas <georgiosmidas@gmail.com>
2023-12-19 17:13:03 +0000

    Minor UI improvments

--- 1_🏠_1._Home.py
+++ 1_🏠_1._Home.py
@@ -115,18 +115,7 @@ else:
                 if submitted:
                     st.toast("Updated Income table 💰", icon="🎉")
 
-            if len(st.session_state.income_data["Date"]) < 1:
-                add_sample_income_data = st.button("Add sample income data", help="Imports sample data. Removes any existing data.")
-                if add_sample_income_data:
-                    st.session_state.income_data = pd.DataFrame({
-                        'Date': ["2023-01-01", "2023-02-01", "2023-03-01", "2023-04-01", "2023-05-01", "2023-06-01", "2023-07-01", "2023-08-01"],
-                        'Net income': [2000, 2000, 1900, 1900, 2000, 2100, 2100, 2200],
-                        'Health insurance': [0, 100, 100, 100, 110, 110, 110, 110],
-                        'Pension': [0, 0, 0, 120, 240, 240, 240, 200],
-                        'Bonus': [0, 0, 0, 500, 0, 0, 200, 0],
-                    })
-                    st.toast("Imported sample data", icon="🎉")
-            else:
+            if len(st.session_state.income_data["Date"]) > 1:
                 income_chart_data = pd.DataFrame(
                     st.session_state.income_data,
                     columns=["Date", "Net income", "Health insurance", "Pension", "Bonus"]
@@ -139,37 +128,36 @@ else:
                     # color=["#8ec127", "#00aedb", "#a200ff", "#f47835"]
                 )
 
+            if len(st.session_state.income_data) > 0:
                 # Store last income for use in budgeting
                 if st.session_state.income_data.tail(1)["Net income"].any():
                     last_net_income = int(st.session_state.income_data.tail(1)["Net income"].item())
                     st.session_state.last_net_income = last_net_income
-
                 st.write("##### Monthly Average")
-                if len(st.session_state.income_data) > 1:
-                    for key in st.session_state.income_data:
-                        if key in "Date":
-                            pass
-                        elif key in "Net income":
-                            values_list = st.session_state.income_data[key]
-                            avg_net_income = sum(values_list)/len(values_list)
-                        elif key in "Health insurance":
-                            values_list = st.session_state.income_data[key]
-                            avg_health_ins = sum(values_list)/len(values_list)
-                        elif key in "Pension":
-                            values_list = st.session_state.income_data[key]
-                            avg_income_pension = sum(values_list)/len(values_list)
-                        elif key in "Bonus":
-                            values_list = st.session_state.income_data[key]
-                            avg_income_bonus = sum(values_list)/len(values_list)
-                        else:
-                            pass
-                    income_averages = pd.DataFrame({
-                        'Net income': [avg_net_income],
-                        'Health insurance': [avg_health_ins],
-                        'Pension': [avg_income_pension],
-                        'Bonus': [avg_income_bonus],
-                    })
-                    st.dataframe(income_averages, hide_index=True, use_container_width=True)
+                for key in st.session_state.income_data:
+                    if key in "Date":
+                        pass
+                    elif key in "Net income":
+                        values_list = st.session_state.income_data[key]
+                        avg_net_income = sum(values_list)/len(values_list)
+                    elif key in "Health insurance":
+                        values_list = st.session_state.income_data[key]
+                        avg_health_ins = sum(values_list)/len(values_list)
+                    elif key in "Pension":
+                        values_list = st.session_state.income_data[key]
+                        avg_income_pension = sum(values_list)/len(values_list)
+                    elif key in "Bonus":
+                        values_list = st.session_state.income_data[key]
+                        avg_income_bonus = sum(values_list)/len(values_list)
+                    else:
+                        pass
+                income_averages = pd.DataFrame({
+                    'Net income': [avg_net_income],
+                    'Health insurance': [avg_health_ins],
+                    'Pension': [avg_income_pension],
+                    'Bonus': [avg_income_bonus],
+                })
+                st.dataframe(income_averages, hide_index=True, use_container_width=True)
 
                 if st.toggle("Show all income data"):
                     st.dataframe(st.session_state.income_data, hide_index=True, use_container_width=True)
@@ -228,20 +216,7 @@ else:
                 if submitted:
                     st.toast("Updated Spending 💸", icon="🎉")
 
-            if len(st.session_state.spend_data["Date"]) < 1:
-                add_sample_spend_data = st.button("Add sample spending data", help="Imports sample data. Removes any existing data.")
-                if add_sample_spend_data:
-                    st.session_state.spend_data = pd.DataFrame({
-                        "Date": ["2023-01-01", "2023-02-01", "2023-03-01", "2023-04-01", "2023-05-01", "2023-06-01", "2023-07-01", "2023-08-01", "2023-09-01"],
-                        "Bills": [800,800,800,800,800,900,900,900,900],
-                        "Pension": [100,100,100,100,100,120,120,120,120],
-                        "Food + Fun": [400,430,542,610,490,580,550,520,500],
-                        "Investments": [0,0,100,100,150,100,130,100,100],
-                        "Shopping": [50,80,100,10,250,30,130,40,0],
-                        "Travel": [0,0,0,130,150,0,0,90,100],
-                    })
-                    st.toast("Imported sample data", icon="🎉")
-            else:
+            if len(st.session_state.spend_data["Date"]) > 1:
                 spend_chart_data = pd.DataFrame(
                     st.session_state.spend_data,
                     columns=["Date", "Bills", "Pension", "Food+Fun", "Investments", "Shopping", "Travel",],
@@ -255,40 +230,40 @@ else:
                     # color=["#ff6961", "#f10ff8", "#f1aff8", "#d7740d", "#08cad1", "#0d70d7"],
                 )
 
+            if len(st.session_state.spend_data) > 0:
                 st.write("##### Monthly Average")
-                if len(st.session_state.spend_data) > 1:
-                    for key in st.session_state.spend_data:
-                        if key in "Date":
-                            pass
-                        elif key in "Bills":
-                            values_list = st.session_state.spend_data[key]
-                            avg_spend_bills = sum(values_list)/len(values_list)
-                        elif key in "Pension":
-                            values_list = st.session_state.spend_data[key]
-                            avg_spend_pension = sum(values_list)/len(values_list)
-                        elif key in "Food + Fun":
-                            values_list = st.session_state.spend_data[key]
-                            avg_spend_food = sum(values_list)/len(values_list)
-                        elif key in "Investments":
-                            values_list = st.session_state.spend_data[key]
-                            avg_spend_invest = sum(values_list)/len(values_list)
-                        elif key in "Shopping":
-                            values_list = st.session_state.spend_data[key]
-                            avg_spend_shop = sum(values_list)/len(values_list)
-                        elif key in "Travel":
-                            values_list = st.session_state.spend_data[key]
-                            avg_spend_travel = sum(values_list)/len(values_list)
-                        else:
-                            pass
-                    spend_averages = pd.DataFrame({
-                        "Bills": [avg_spend_bills],
-                        "Pension": [avg_spend_pension],
-                        "Food + Fun": [avg_spend_food],
-                        "Investments": [avg_spend_invest],
-                        "Shopping": [avg_spend_shop],
-                        "Travel": [avg_spend_travel],
-                    })
-                    st.dataframe(spend_averages, hide_index=True, use_container_width=True)
+                for key in st.session_state.spend_data:
+                    if key in "Date":
+                        pass
+                    elif key in "Bills":
+                        values_list = st.session_state.spend_data[key]
+                        avg_spend_bills = sum(values_list)/len(values_list)
+                    elif key in "Pension":
+                        values_list = st.session_state.spend_data[key]
+                        avg_spend_pension = sum(values_list)/len(values_list)
+                    elif key in "Food + Fun":
+                        values_list = st.session_state.spend_data[key]
+                        avg_spend_food = sum(values_list)/len(values_list)
+                    elif key in "Investments":
+                        values_list = st.session_state.spend_data[key]
+                        avg_spend_invest = sum(values_list)/len(values_list)
+                    elif key in "Shopping":
+                        values_list = st.session_state.spend_data[key]
+                        avg_spend_shop = sum(values_list)/len(values_list)
+                    elif key in "Travel":
+                        values_list = st.session_state.spend_data[key]
+                        avg_spend_travel = sum(values_list)/len(values_list)
+                    else:
+                        pass
+                spend_averages = pd.DataFrame({
+                    "Bills": [avg_spend_bills],
+                    "Pension": [avg_spend_pension],
+                    "Food + Fun": [avg_spend_food],
+                    "Investments": [avg_spend_invest],
+                    "Shopping": [avg_spend_shop],
+                    "Travel": [avg_spend_travel],
+                })
+                st.dataframe(spend_averages, hide_index=True, use_container_width=True)
                 st.write("_The monthly average is a guide for setting accurate values in Monthly Outflows._")
 
                 if st.toggle("Show all spending data"):
